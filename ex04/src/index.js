898dd8dd0618fd1479a122e6d5848d76760d5379
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-30 08:55:35 +0200

    Add files via upload

---
+++ ex04/src/index.js
@@ -0,0 +1,23 @@
+const BAKING_TEMPERATURES = {
+   cake: { low: 180, high: 250 },
+   bread: { low: 150, high: 220 },
+   pie: { low: 200, high: 230 }
+};
+
+function main(baking) {
+
+// Only change code below this line
+
+const { lowBread: bread.low } = baking;
+
+const { highBread: bread.high } = baking;
+
+return {
+	lowBread,
+	highBread
+};
+// Only change code above this line
+}
+
+console.log(main(BAKING_TEMPERATURES));
+module.exports = main;
